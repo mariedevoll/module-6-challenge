0b459c1287b728c71131e2d7ddd25d31a786dffe
mariedevoll <marie.devoll@gmail.com>
2024-04-08 21:17:55 -0400

    working my way through the challenge

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,8 +8,138 @@ const todayContainer = document.querySelector("#today");
 const forecastContainer = document.querySelector("#forecast");
 const weatherHistoryContainer = document.querySelector("#weather-history");
 
-function fetchCoordinates(search) {
+const displayCurrentWeather = (city, weatherData) => {
+    const date = dayjs().format("M/D/YYYY");
+    const tempF = weatherData.main.temp;
+    const windMph = weatherData.wind.speed;
+    const humidity = weatherData.main.humidity;
+    const iconUrl = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const iconDescription = weatherData.weather[0].description || "No Description";
+    
 
+    const card = document.createElement("div");
+    const cardBody = document.createElement("div");
+    const heading = document.createElement("h3");
+    const weatherIcon = document.createElement("img");
+    const temperatureElement = document.createElement("p");
+    const windElement = document.createElement("p");
+    const humidityElement = document.createElement("p");
+
+    card.setAttribute("class", "card bg-dark text-white");
+    cardBody.setAttribute("class", "card-body");
+    card.append(cardBody);
+
+    heading.setAttribute("class", "h3 card-title");
+    temperatureElement.setAttribute("class", "card-text");
+    windElement.setAttribute("class", "card-text");
+    humidityElement.setAttribute("class", "card-text");
+
+    heading.textContent = `${city} (${date})`;
+    weatherIcon.setAttribute("src", iconUrl);
+    weatherIcon.setAttribute("alt", iconDescription);
+    heading.append(weatherIcon);
+    temperatureElement.textContent = `Temperature🌡️: ${tempF} °F`;
+    windElement.textContent = `Wind 🌬️: ${windMph} MPH`;
+    humidityElement.textContent = `Humidity💦: ${humidity} %`;
+    cardBody.append(heading, temperatureElement, windElement, humidityElement);
+
+    todayContainer.innerHTML = " ";
+    todayContainer.append(card);
+}
+
+const displayForecast = (weatherData) => {
+    const startDate = dayjs().add(1, "day").startOf("day").unix();
+    const endDay = dayjs().add(6, "day").startOf("day").unix();
+
+    const headingColumn = document.createElement("div");
+    const heading = document.createElement("h3");
+    headingColumn.setAttribute("class", "col-12");
+    heading.textContent = "5 Day Forecast:";
+    headingColumn.append(heading);
+
+    forecastContainer.innerHTML = " ";
+    forecastContainer.append(headingColumn);
+
+    console.log(startDate)
+    console.log(endDate)
+
+    for(let index = 0; index < weatherData.length; index++) {
+        console.log(weatherData[index].dt);
+        if(weatherData[index].dt >= startDate && weatherData[index].dt < endDate) {
+            console.log(weatherData[index].dt_txt.slice(11, 13));
+            if(weatherData[index].dt_txt.slice(11,13) === "12") {
+                console.log(weatherData[index]);
+            }
+        }
+    }
+}
+
+const fetchWeather = (location) => { 
+    const latitude = location.lat;
+    const longitude = location.lon;
+
+    const city = location.name;
+
+    const apiURL = `${weatherAPIBaseURL}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherAPIKey}`
+    fetch(apiURL).then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+
+        displayCurrentWeather(city, data.list[0])
+        displayForecast(data.list);
+    }).catch(function(error){
+        console.log(error);
+    })
+}
+
+const createSearchHistory = ( ) => {
+    weatherHistoryContainer.innerHTML = "";
+    for(let index = 0; index < searchHistoryForWeather.length; index++) {
+        const buttonElement = document.createElement("button");
+        buttonElement.setAttribute("id" , "city-button");
+        buttonElement.setAttribute("type" , "button");
+        buttonElement.setAttribute("class" , "btn btn-secondary");
+        buttonElement.setAttribute("aria-controls" , "today forecast");
+        buttonElement.classList.add("history-button");
+        buttonElement.setAttribute("data-search", searchHistoryForWeather[index]); 
+        buttonElement.textContent = searchHistoryForWeather[index];
+        weatherHistoryContainer.append(buttonElement);
+    }
+}
+
+const appendWeatherHistory = (search) => {
+    if(searchHistoryForWeather.indexOf(search) !== -1) {
+        return;
+    }
+    searchHistoryForWeather.push(search);
+    localStorage.setItem("weatherHistory", JSON.stringify(searchHistoryForWeather));
+    createSearchHistory();
+ }
+
+const fetchCoordinates = (search) => {
+
+    const url = `${weatherAPIBaseURL}//geo/1.0/direct?q=${search}&appid=${weatherAPIKey}`;
+     fetch(url)
+    .then(function(response) {
+        return response.json();
+    }).then(function(data){
+        
+        if(!data[0]){
+            alert('City not found :/');
+        } else {
+            console.log(data);
+            console.log(search);
+            appendWeatherHistory(search);
+            fetchWeather(data[0]);
+        }
+
+        // const latitude = data[0].lat;
+        // const longitude = data[0].lon;
+        // console.log(data, latitude, longitude);
+    }).catch(function(error){
+        console.log(error);
+    });
 
 }
 
@@ -20,6 +150,28 @@ const handleSearchFormSubmit = (event) => {
     if(search) {
         fetchCoordinates(search);
     }
+    searchInput.value = "";
 }
 
-searchForm.addEventListener("submit", )
+const initializeSearchHistory = () => {
+    const storedWeatherHistory = JSON.parse(localStorage.getItem("weatherHistory"));
+    if(storedWeatherHistory) {
+        searchHistoryForWeather = storedWeatherHistory;
+    }
+    createSearchHistory();
+}
+
+const handleSearchHistoryClick = (event) => {
+    console.log(event.target)
+    if(!event.target.matches(".history-button")) {
+        return;
+    }
+    const buttonElement = event.target;
+
+    const search = buttonElement.getAttribute("data-search");
+    fetchCoordinates(search);
+}
+// events!
+initializeSearchHistory();
+searchForm.addEventListener("submit", handleSearchFormSubmit);
+weatherHistoryContainer.addEventListener("click", handleSearchHistoryClick);
